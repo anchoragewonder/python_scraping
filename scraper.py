9e6ec5ce8a1817c81bc9024660da7505a8da93ee
anchoragewonder <anchoragewonder@gmail.com>
2021-01-19 17:58:23 -0500

    scraping data into similar json

--- scraper.py
+++ scraper.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup as bs
 from urllib.request import Request, urlopen
 from collections import defaultdict
+import json
 
-
+soccer_data = {}
+soccer_data["matches"] = []
 soccer_dict = defaultdict(list)
 
 r = Request(
@@ -20,10 +22,29 @@ soup = bs(page_html, "html.parser")
 
 for games in soup.find_all("h4", class_="fixres__header2"):
 
+    date = games.text
     sibling = games.nextSibling
+
     while sibling is not None and sibling.name != "h4":
         if sibling.name == "div":
-            soccer_dict[games.text].append(sibling)
+
+            home_team = sibling.find("span", class_="matches__participant--side1")
+            away_team = sibling.find("span", class_="matches__participant--side2")
+            score = sibling.find_all("span", class_="matches__teamscores-side")
+
+            homescore = score[0].text
+            awayscore = score[1].text
+
+            soccer_data["matches"].append(
+                {
+                    "date": games.text,
+                    "team1": home_team.text,
+                    "team2": away_team.text,
+                    "score": {"ft": [homescore, awayscore]},
+                }
+            )
+
+            soccer_dict[date].append(sibling)
         sibling = sibling.nextSibling
 
-print(soccer_dict)
+print(soccer_data)
